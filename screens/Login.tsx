2911bf8aa914df3b3a06cb3aa43d33a0fee12fb8
Christine Gregotski <tuj39467@temple.edu>
2021-09-28 18:34:42 -0400

    Background image works on login screen

--- screens/Login.tsx
+++ screens/Login.tsx
@@ -2,24 +2,52 @@
 import React from "react";
 
 import { View, ImageBackground, Text, StyleSheet } from "react-native";
+/*
+const image={uri:
+  'https://media.istockphoto.com/photos/small-kittens-picture-id516230467?k=6&m=516230467&s=612x612&w=0&h=Exd6B-5vXxg-4t_t_USCDGqKO6d-1KCmQkS_smprKnI='
+ };
 
-const Home = () => {
-  return (
-    <ImageBackground 
-    source={{uri:
-     'https://media.istockphoto.com/photos/small-kittens-picture-id516230467?k=6&m=516230467&s=612x612&w=0&h=Exd6B-5vXxg-4t_t_USCDGqKO6d-1KCmQkS_smprKnI='
-    }}>
-     
-      
+const Login = () => {
+ // return (
+  //  <ImageBackground 
+    
+     <View style={styles.container}>
+      <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+      <Text style={styles.text}>Inside</Text>
     </ImageBackground>
-  );
+  </View>
+      
+  //  </ImageBackground>
+  //);
 };
 
 const styles = StyleSheet.create({
-  img: {
-    flex: 1
-  },
-})
+  //img: {
+  //  flex: 1
+    //const styles = StyleSheet.create({
+ 
+ 
+     container: {
+       flex: 1,
+     },
+     image: {
+       flex: 1,
+       justifyContent: "center"
+     },
+     text: {
+       color: "white",
+       fontSize: 42,
+       lineHeight: 84,
+       fontWeight: "bold",
+       textAlign: "center",
+       backgroundColor: "#000000c0"
+     }
+   });*/
+ // },
+//})
+
+
+
 
 
 /*
@@ -34,4 +62,4 @@ const Home = () => {
 /// <Text>This is the home screen</Text>
 
 */
-export default Home;
+//export default Login;
