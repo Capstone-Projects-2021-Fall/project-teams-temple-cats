17e046883ebdfa9521463b5c2215b11c89202514
Christine Gregotski <tuj39467@temple.edu>
2021-09-29 16:22:07 -0400

    Resolving merge conflicts

--- App.tsx
+++ App.tsx
@@ -1,15 +1,7 @@
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import {Button, SafeAreaView, View, ImageBackground, Text, StyleSheet, Alert } from "react-native";
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import Login from './screens/Login'
-const image={uri:
-  'https://media.istockphoto.com/photos/small-kittens-picture-id516230467?k=6&m=516230467&s=612x612&w=0&h=Exd6B-5vXxg-4t_t_USCDGqKO6d-1KCmQkS_smprKnI='
- };
-
-<<<<<<< HEAD
-const App = () => {
-  return (
-      <Login></Login>
-=======
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
@@ -18,7 +10,7 @@ import LoginAuthentication from './components/LoginAuthentication';
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
-
+  <Login></Login>
   if (!isLoadingComplete) {
     return null;
   } else {
@@ -27,80 +19,6 @@ export default function App() {
         <LoginAuthentication/>
         <StatusBar />
       </SafeAreaProvider>
->>>>>>> 88dc6ce34c9ba9a18c585dd9f3bf4574d39241d2
     );
   }
-  export default App
-    /*<SafeAreaView style={styles.container}>
-      <View style={styles.container}>
-        <ImageBackground source={image} resizeMode="cover" style={styles.image}>
-          <Text style={styles.text}>Temple Cats</Text>
-        </ImageBackground>
-      </View>
-      
-      <View>
-        <Button
-          title="Sign in"
-          color="#8b0000"
-          onPress={() => Alert.alert('Redirecting you to Facebook')}
-        />
-      </View>
-    
-      <View>
-        <Separator />
-      </View>
-
-      <View>
-      <Button 
-          title="Create an account"
-          color="#8b0000"
-          onPress={() => Alert.alert('Redirecting you to Facebook')}
-        />
-      </View>
-    </SafeAreaView>
-  );
 }
-
-const Separator = () => (
-  <View style={styles.separator} />
-);
-
-const styles = StyleSheet.create({
-    container: {
-       flex: 1,
-     },
-     image: {
-       flex: 1,
-       justifyContent: "center"
-     },
-
-     title: {
-      textAlign: 'center',
-      marginVertical: 8,
-      backgroundColor: "#2f4f4f"
-      
-    },
-  fixToText: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-     text: {
-       color: "white",
-       fontSize: 42,
-       lineHeight: 84,
-       fontWeight: "bold",
-       textAlign: "center",
-       backgroundColor: "#8b0000"
-     },
-
-     separator: {
-      marginVertical: 8,
-      borderBottomColor: '#737373',
-      borderBottomWidth: StyleSheet.hairlineWidth,
-    },
-   });
-   
-export default App;*/
-/* <Text style={styles.title}>
-Sign in
-</Text>*/
