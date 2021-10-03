b894722d732118761f1b038e68b3de3004ddd7c0
Christian Davis <Phaceial@gmail.com>
2021-10-03 12:15:48 -0400

    get rid of unecessary boiler plate code and add the necessary library

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,35 +1,7 @@
-import * as React from 'react';
-import { StyleSheet } from 'react-native';
-import CatMap from '../components/CatMap';
-
-import EditScreenInfo from "../components/EditScreenInfo";
-import { Text, View } from "../components/Themed";
+import * as React from "react";
+import CatMap from "../components/CatMap";
 import { RootTabScreenProps } from "../types";
 
 export default function HomeScreen({ navigation }: RootTabScreenProps<"Home">) {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Home/Map</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <CatMap/>
-      <EditScreenInfo path="/screens/Home.tsx" />
-    </View>
-  );
+  return <CatMap />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
-  },
-});
