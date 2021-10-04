de4a92063de94295c59510bd55f5bfe18eba6533
Christine Gregotski <tuj39467@temple.edu>
2021-10-03 20:13:52 -0400

    Added Form component to create fields for CatForm.

--- App.tsx
+++ App.tsx
@@ -5,20 +5,47 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
-import Login from './screens/Login';
+//import Login from './screens/Login';
+//import CatForm from './components/Form.tsx/CatForm'
+import { CatForm } from "c:/Users/cgreg/project-teams-temple-cats/components/Form.tsx/CatForm"
 
-export default function App() {
-  const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
 
+
+
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="mt-3">
+        {<CatForm/>}
+      </div>
+    );
+  }
+}
+
+export default App;
+
+//export default function App() {
+  //const isLoadingComplete = useCachedResources();
+  //const colorScheme = useColorScheme();
+
+
+
+
+
+
+/*
   if (!isLoadingComplete) {
     return null;
   } else {
     return (
       <SafeAreaProvider>
         <Login/>
+        <CatForm/>
         <StatusBar />
       </SafeAreaProvider>
     );
   }
-}
+}*/
+//}
+
---
+++ components/Form.tsx/CatForm.tsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+import { Form } from "./Form";
+import { Field } from "./Field";
+
+export const CatForm: React.SFC = () => {
+  return (
+    <Form
+      action="http://localhost:4351/api/contactus"
+      render={() => (
+        <React.Fragment>
+          <div className="alert alert-info" role="alert">
+            Enter the information below and we'll get back to you as soon as we
+            can.
+          </div>
+          <Field id="name" label="Name" />
+          <Field id="email" label="Email" />
+          <Field
+            id="reason"
+            label="Reason"
+            editor="dropdown"
+            options={["", "Marketing", "Support", "Feedback", "Jobs"]}
+          />
+          <Field id="notes" label="Notes" editor="multilinetextbox" />
+        </React.Fragment>
+      )}
+    />
+  );
+};
