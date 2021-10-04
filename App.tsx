ae10b73e2800450f495a411e602c3db99db07440
Christine Gregotski <tuj39467@temple.edu>
2021-10-04 10:24:42 -0400

    Added Form and service for cat form.

--- App.tsx
+++ App.tsx
@@ -1,23 +1,65 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { Component } from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Switch, Route, Link } from "react-router-dom";
+import './App.css';
+import AddTutorial from "./components/add-Cat-Data";
+import TutorialsList from "./components/Form-List";
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
-//import Login from './screens/Login';
-//import CatForm from './components/Form.tsx/CatForm'
-import { CatForm } from "c:/Users/cgreg/project-teams-temple-cats/components/Form.tsx/CatForm"
+import Login from './screens/Login';
 
 
-
-
-
-class App extends React.Component {
-  public render() {
+/*export default function App() {
+  const isLoadingComplete = useCachedResources();
+  const colorScheme = useColorScheme();
+  if (!isLoadingComplete) {
+    return null;
+  } else {
     return (
-      <div className="mt-3">
-        {<CatForm/>}
+      <SafeAreaProvider>
+        <Login/>
+        <StatusBar />
+      </SafeAreaProvider>
+    );
+  }
+}*/
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-expand navbar-dark bg-dark">
+          <Link to={"/tutorials"} className="navbar-brand">
+            bezKoder
+          </Link>
+          <div className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link to={"/tutorials"} className="nav-link">
+                Tutorials
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to={"/add"} className="nav-link">
+                Add
+              </Link>
+            </li>
+          </div>
+        </nav>
+
+        <div className="container mt-3">
+          <h2>React Typescript Firebase example</h2>
+          <Switch>
+            <Route exact path={["/", "/tutorials"]} component={TutorialsList} />
+            <Route exact path="/add" component={AddTutorial} />
+          </Switch>
+        </div>
       </div>
     );
   }
@@ -25,27 +67,6 @@ class App extends React.Component {
 
 export default App;
 
-//export default function App() {
-  //const isLoadingComplete = useCachedResources();
-  //const colorScheme = useColorScheme();
 
 
-
-
-
-
-/*
-  if (!isLoadingComplete) {
-    return null;
-  } else {
-    return (
-      <SafeAreaProvider>
-        <Login/>
-        <CatForm/>
-        <StatusBar />
-      </SafeAreaProvider>
-    );
-  }
-}*/
-//}
 
---
+++ Types/Cat-type.ts
@@ -0,0 +1,6 @@
+export default interface CatData {
+    key?: string | null,
+    title: string,
+    description: string,
+    published?: boolean,
+  }
