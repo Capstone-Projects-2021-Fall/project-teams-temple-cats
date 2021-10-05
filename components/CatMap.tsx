f458bd1d11f8ca59c9a31e4b37ced0df7e2c4c36
Christian Davis <Phaceial@gmail.com>
2021-10-05 12:57:45 -0400

    Add Button component and firebase types

--- components/CatMap.tsx
+++ components/CatMap.tsx
@@ -1,107 +1,107 @@
-import React, { useEffect, useState } from 'react';
-import { StyleSheet } from 'react-native';
-import MapView, { Marker, Region } from 'react-native-maps';
-import { View } from './Themed';
-import firebase from '../utils/firebase';
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Button } from "react-native";
+import MapView, { Marker, Region } from "react-native-maps";
+import { View } from "./Themed";
+import firebase from "../utils/firebase";
 
 interface Markers {
-  id: number
-  latitude: number
-  longitude: number
+  id: number;
+  latitude: number;
+  longitude: number;
 }
 
 //Stores cat pins into array
 
- var catObject = {};
+var catObject = {};
 
-  firebase.database().ref('Pins').on('value', (snap) => {
+firebase
+  .database()
+  .ref("Pins")
+  .on("value", (snap) => {
     catObject = snap.exportVal();
   });
 
-  const cat_names = Object.keys(catObject);
+const cat_names = Object.keys(catObject);
 
 function getLatitude(x: String) {
-
   var catLatitude = 0; //initializes cat lattidue
 
   //Builds queryString
 
-  let queryString = 'Pins/';
+  let queryString = "Pins/";
   queryString += x;
-  queryString += '/Location/lat';
+  queryString += "/Location/lat";
 
   console.log(queryString);
 
-  firebase.database().ref(queryString).on('value', (snap) => {
-    catLatitude = snap.exportVal();
-  });
+  firebase
+    .database()
+    .ref(queryString)
+    .on("value", (snap) => {
+      catLatitude = snap.exportVal();
+    });
 
   console.log(catLatitude);
   return catLatitude;
-  
 }
 
 function getLongitude(x: string) {
   var catLongitude = 0;
 
-  let queryString = 'Pins/';
+  let queryString = "Pins/";
   queryString += x;
-  queryString += '/Location/lng';
+  queryString += "/Location/lng";
 
   console.log(queryString);
 
-  firebase.database().ref(queryString).on('value', (snap) => {
-    catLongitude = snap.exportVal();
-  });
+  firebase
+    .database()
+    .ref(queryString)
+    .on("value", (snap) => {
+      catLongitude = snap.exportVal();
+    });
 
   console.log(catLongitude);
   return catLongitude;
-
 }
 
 export default function CatMap() {
-
   const [markers, setMarkers] = useState<Markers[]>([
     { id: 0, latitude: 53.91326738786109, longitude: 27.523712915343737 },
-  ])
+  ]);
 
   const region: Region = {
     latitude: 39.9812,
     longitude: -75.1497,
     latitudeDelta: 0.015,
     longitudeDelta: 0.0121,
-  }
+  };
 
   //Avoids uneccasary rerenders of code
 
   const generateMarkers = React.useCallback((lat: number, long: number) => {
-
-    const markersArray = []
+    const markersArray = [];
 
     //Passes cat pins to retrieve longitude and lattiude and pushes to MarkersArray
 
     for (let i = 0; i < cat_names.length; i++) {
-
       markersArray.push({
         id: i,
         latitude: getLatitude(cat_names[i]),
         longitude: getLongitude(cat_names[i]),
-      })
+      });
     }
-   
-    setMarkers(markersArray)
-  }, [])
+
+    setMarkers(markersArray);
+  }, []);
 
   useEffect(() => {
-    generateMarkers(region.latitude, region.longitude)
-  }, [])
-
+    generateMarkers(region.latitude, region.longitude);
+  }, []);
 
   return (
-
     <View style={styles.container}>
       <MapView
-
         style={styles.map}
         region={{
           latitude: 39.9812,
@@ -109,7 +109,6 @@ export default function CatMap() {
           latitudeDelta: 0.015,
           longitudeDelta: 0.0121,
         }}
-
       >
         {markers.map((item) => (
           <Marker
@@ -117,9 +116,20 @@ export default function CatMap() {
             coordinate={{
               latitude: item.latitude,
               longitude: item.longitude,
-            }}></Marker>
+            }}
+          ></Marker>
         ))}
       </MapView>
+
+      <View style={styles.buttonContainer}>
+        <Button
+          color="#8b0000"
+          title="Add a Pin"
+          onPress={() => {
+            alert("test");
+          }}
+        />
+      </View>
     </View>
   );
 }
@@ -127,17 +137,17 @@ export default function CatMap() {
 const styles = StyleSheet.create({
   container: {
     ...StyleSheet.absoluteFillObject,
-    height: '100%',
-    width: '100%',
-    justifyContent: 'flex-end',
-    alignItems: 'center',
+    height: "100%",
+    width: "100%",
+    justifyContent: "flex-end",
+    alignItems: "center",
   },
   map: {
     ...StyleSheet.absoluteFillObject,
   },
   buttonContainer: {
-    position: 'absolute',
+    position: "absolute",
     top: 50,
-    left: 20
-  }
+    left: 20,
+  },
 });
