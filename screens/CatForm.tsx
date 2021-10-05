1d817c765eec792e89b39740889a0f714d6b6f90
Christine Gregotski <tuj39467@temple.edu>
2021-10-05 19:01:32 -0400

    Added CatForm.tsx screen

--- screens/CatForm.tsx
+++ screens/CatForm.tsx
@@ -1,25 +1,94 @@
 import React from "react";
-import { SafeAreaView, StyleSheet, TextInput ,Text} from "react-native";
+import { SafeAreaView, StyleSheet, TextInput ,Text, ScrollView, StatusBar, View, Button, Alert} from "react-native";
 
-const UselessTextInput = () => {
-  const [text, onChangeText] = React.useState("Useless Text");
-  const [number, onChangeNumber] = React.useState(null);
+
+const Information = () => {
+  const [text1, media] = React.useState("");
+  const [text2, uniqueFeatures] = React.useState("");
+  const [text3, possibleName] = React.useState("");
+  const [text4, behavior] = React.useState("");
+  const [text5, color] = React.useState("");
+  const [text6, ageEstimate] = React.useState("");
+  const [text7, condition] = React.useState("");
+  const [text8, friendliness] = React.useState("");
+  const [value, onChangeText] = React.useState('Useless Multiline Placeholder');
+  const [text9, additionalComments] = React.useState("");
+  const [text10, pinID] = React.useState("");
+  //const [number, onChangeNumber] = React.useState(null);
 
   return (
     <SafeAreaView>
+      <ScrollView>
+     
       <Text style={styles.text}>Cat Form</Text>
+      <Text style={{ fontSize: 20, fontWeight:"bold" }}>    Media</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={media}
+        value={text1}
+      />
+      <Text style={{ fontSize: 20, fontWeight:"bold" }}>    Unique Features</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={uniqueFeatures}
+        value={text2}
+      />
+      <Text style={{ fontSize: 20, fontWeight:"bold" }}>    Possible Name</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={possibleName}
+        value={text3}
+      />
+      <Text style={{ fontSize: 20, fontWeight:"bold" }}>    Behavior</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={behavior}
+        value={text4}
+      />
       <Text style={{ fontSize: 20, fontWeight:"bold" }}>    Color</Text>
       <TextInput
         style={styles.input}
-        onChangeText={onChangeText}
-        value={text}
+        onChangeText={color}
+        value={text5}
       />
-      <Text style={{ fontSize: 20, fontWeight:"bold" }}>    Size</Text>
+      <Text style={{ fontSize: 20, fontWeight:"bold" }}>    Age Estimate</Text>
       <TextInput
         style={styles.input}
-        onChangeText={onChangeText}
-        value={text}
+        onChangeText={ageEstimate}
+        value={text6}
       />
+      <Text style={{ fontSize: 20, fontWeight:"bold" }}>    Condition</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={condition}
+        value={text7}
+      />
+      <Text style={{ fontSize: 20, fontWeight:"bold" }}>    Friendliness</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={friendliness}
+        value={text8}
+      />
+      <Text style={{ fontSize: 20, fontWeight:"bold" }}>    Pin ID</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={additionalComments}
+        value={text9}
+      />
+      <Text style={{ fontSize: 20, fontWeight:"bold" }}>    Pin ID</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={pinID}
+        value={text10}
+      />
+       <Button
+        title="Submit"
+        color="#8b0000"
+        onPress={() => Alert.alert('Submitted Successfully')}
+      />
+
+      
+      </ScrollView>
     </SafeAreaView>
   );
 };
@@ -31,6 +100,13 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     padding: 10,
   },
+  container: {
+    flex: 1,
+    paddingTop: StatusBar.currentHeight,
+  },
+  scrollView: {
+    marginHorizontal: 20,
+  },
   text: {
     color: "white",
     fontSize: 42,
@@ -40,7 +116,6 @@ const styles = StyleSheet.create({
     backgroundColor: "#8b0000",
     marginBottom: 50,
   },
- 
 });
 
-export default UselessTextInput;
+export default Information;
