b6756c4ed519f97e9b0f589732cec4ca821bab47
Christian Davis <Phaceial@gmail.com>
2021-10-07 19:04:55 -0400

    Correct Solution for TEM-44

--- App.tsx
+++ App.tsx
@@ -1,14 +1,10 @@
 import React from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import Navigation from "./navigation/index";
-import LoginScreen from "./screens/Login"
 
 import useCachedResources from "./hooks/useCachedResources";
 import useColorScheme from "./hooks/useColorScheme";
-import { AuthContext } from "./context/FirebaseAuthContext";
 import { AuthProvider } from "./context/FirebaseAuthProvider";
 import CatMap from "./components/CatMap";
-import Pins from "./components/Pins";
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
@@ -20,7 +16,7 @@ export default function App() {
     return (
       <SafeAreaProvider>
         <AuthProvider>
-          <Pins/>
+          <CatMap />
         </AuthProvider>
       </SafeAreaProvider>
     );
--- components/CatMap.tsx
+++ components/CatMap.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import { StyleSheet } from "react-native";
 import MapView, { Marker } from "react-native-maps";
 import { View } from "./Themed";
+import firebase from "../utils/firebase";
 
 /**
  * Function that renders the Cat Map component, including the map and all it's children (e.g. pins/markers).
@@ -10,6 +11,26 @@ import { View } from "./Themed";
  * @returns {JSX.Element} JSX element of the map
  */
 export default function CatMap() {
+  const [markers, setMarkers] = React.useState<any>([]);
+  let markersArray = [];
+
+  React.useEffect(() => {
+    var reference = firebase.database().ref("Pins/");
+    let pin;
+    reference.on("value", (snapshot) => {
+      let items = snapshot.val();
+      let newState = [];
+      for (let item in items) {
+        newState.push({
+          id: item,
+          lat: items[item].Location.lat,
+          lng: items[item].Location.lng,
+        });
+      }
+      setMarkers(newState);
+    });
+  }, []);
+
   return (
     <View style={styles.container}>
       <MapView
@@ -20,16 +41,18 @@ export default function CatMap() {
           latitudeDelta: 0.015,
           longitudeDelta: 0.0121,
         }}
+        provider={"google"}
       >
-        <Marker
-          key="testkey"
-          coordinate={{
-            latitude: 39.9812,
-            longitude: -75.1497,
-          }}
-          title="testtitle"
-          description="testdescription"
-        />
+        {markers?.map((item, index) => (
+          <Marker
+            key={index}
+            title={item.id}
+            coordinate={{
+              latitude: item.lat,
+              longitude: item.lng,
+            }}
+          ></Marker>
+        ))}
       </MapView>
     </View>
   );
