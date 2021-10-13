465abfb3900ae4a7dbb03abf2c60d52ac68dedd5
Rebecca Robb <tuf87647@temple.edu>
2021-10-13 17:13:31 -0400

    Add image picker

--- screens/CatForm.tsx
+++ screens/CatForm.tsx
@@ -1,91 +1,93 @@
 import firebase from "firebase";
 import React from "react";
 import { SafeAreaView, StyleSheet, TextInput ,Text, ScrollView, StatusBar, View, Button, Alert} from "react-native";
-
+import ImagePickerExample from "../components/ImagePicker"
 
 const CatForm = () => {
-  const [text1, media] = React.useState("");
-  const [text2, uniqueFeatures] = React.useState("");
-  const [text3, possibleName] = React.useState("");
-  const [text4, behavior] = React.useState("");
-  const [text5, color] = React.useState("");
-  const [text6, ageEstimate] = React.useState("");
-  const [text7, condition] = React.useState("");
-  const [text8, eyeColor] = React.useState("");
-  const [text9, friendliness] = React.useState("");
-  const [text10, additionalComments] = React.useState("");
-  const [text11, pinID] = React.useState("");
+  // const [text1, media] = React.useState("");
+  const [uniqueFeatures, setUniqueFeatures] = React.useState("");
+  const [possibleName, setPossibleName] = React.useState("");
+  const [behavior, setBehavior] = React.useState("");
+  const [color, setColor] = React.useState("");
+  const [ageEstimate, setAgeEstimate] = React.useState("");
+  const [condition, setCondition] = React.useState("");
+  const [eyeColor, setEyeColor] = React.useState("");
+  const [friendliness, setFriendliness] = React.useState("");
+  const [additionalComments, setAdditionalComments] = React.useState("");
+  const [pinID, setPinID] = React.useState("");
 
   return (
     <SafeAreaView>
       <ScrollView>
      
       <Text style={styles.text}>Cat Form</Text>
-      <Text style={{ fontSize: 20, fontWeight:"bold" }}>    Media</Text>
+      {/* <Text style={{ fontSize: 20, fontWeight:"bold" }}>    Media</Text>
       <TextInput
         style={styles.input}
         onChangeText={media}
         value={text1}
-      />
+      /> */}
+      <Text style={{ fontSize: 20, fontWeight:"bold" }}>    Media</Text>
+      <ImagePickerExample />
       <Text style={{ fontSize: 20, fontWeight:"bold" }}>    Unique Features</Text>
       <TextInput
         style={styles.input}
-        onChangeText={uniqueFeatures}
-        value={text2}
+        onChangeText={setUniqueFeatures}
+        value={uniqueFeatures}
       />
       <Text style={{ fontSize: 20, fontWeight:"bold" }}>    Possible Name</Text>
       <TextInput
         style={styles.input}
-        onChangeText={possibleName}
-        value={text3}
-      />
+        onChangeText={setPossibleName}
+        value={possibleName}
+     />
       <Text style={{ fontSize: 20, fontWeight:"bold" }}>    Behavior</Text>
       <TextInput
         style={styles.input}
-        onChangeText={behavior}
-        value={text4}
-      />
+        onChangeText={setBehavior}
+        value={behavior}
+       />
       <Text style={{ fontSize: 20, fontWeight:"bold" }}>    Color</Text>
       <TextInput
         style={styles.input}
-        onChangeText={color}
-        value={text5}
+        onChangeText={setColor}
+        value={color}
       />
       <Text style={{ fontSize: 20, fontWeight:"bold" }}>    Age Estimate</Text>
       <TextInput
         style={styles.input}
-        onChangeText={ageEstimate}
-        value={text6}
+        onChangeText={setAgeEstimate}
+        value={ageEstimate}
       />
       <Text style={{ fontSize: 20, fontWeight:"bold" }}>    Condition</Text>
       <TextInput
         style={styles.input}
-        onChangeText={condition}
-        value={text7}
-      />
+        onChangeText={setCondition}
+        value={condition}
+         />
        <Text style={{ fontSize: 20, fontWeight:"bold" }}>    Eye Color</Text>
       <TextInput
         style={styles.input}
-        onChangeText={eyeColor}
-        value={text8}
-      />
+        onChangeText={setEyeColor}
+        value={eyeColor}
+        />
       <Text style={{ fontSize: 20, fontWeight:"bold" }}>    Friendliness</Text>
       <TextInput
         style={styles.input}
-        onChangeText={friendliness}
-        value={text9}
+        onChangeText={setFriendliness}
+        value={friendliness}
       />
       <Text style={{ fontSize: 20, fontWeight:"bold" }}>    Additional Comments</Text>
       <TextInput
         style={styles.input}
-        onChangeText={additionalComments}
-        value={text10}
+        onChangeText={setAdditionalComments}
+        value={additionalComments}
       />
       <Text style={{ fontSize: 20, fontWeight:"bold" }}>    Pin ID</Text>
       <TextInput
         style={styles.input}
-        onChangeText={pinID}
-        value={text11}
+        onChangeText={setPinID}
+        value={pinID}
       />
 
        <Button
@@ -93,35 +95,35 @@ const CatForm = () => {
         color="#8b0000"
         onPress={() => 
         {
-            if (!text1.trim() ||  !text2.trim() || !text3.trim() || !text4.trim() || !text5.trim() || !text6.trim()
-            || !text7.trim() || !text8.trim() || !text9.trim() || !text10.trim() || !text11.trim()) {
+            if ( !uniqueFeatures.trim() || !possibleName.trim() || !behavior.trim() || !color.trim() || !ageEstimate.trim()
+            || !condition.trim() || !eyeColor.trim() || !friendliness.trim() || !additionalComments.trim() || !pinID.trim()) {
             alert('Please fill out all fields');
             return;
             }
             firebase.database().ref('Cats/'+ "110192021-2").set({
-              media: text1,
-              uniqueFeatures: text2,
-              possibleName: text3,
-              behavior: text4,
-              color: text5,
-              ageEstimate: text6,
-              condition: text7,
-              eyeColor: text8,
-              friendliness: text9,
-              additionalComments: text10,
-              pinID: text11
+              // media: text1,
+              uniqueFeatures: uniqueFeatures,
+              possibleName: possibleName,
+              behavior: behavior,
+              color: color,
+              ageEstimate: ageEstimate,
+              condition: condition,
+              eyeColor: eyeColor,
+              friendliness: friendliness,
+              additionalComments: additionalComments,
+              pinID: pinID
             });
-            console.log(text1,
-              text2,
-              text3,
-              text4,
-              text5,
-              text6,
-              text7,
-              text8,
-              text9,
-              text10,
-              text11)
+            console.log(
+              uniqueFeatures,
+              possibleName,
+              behavior,
+              color,
+              ageEstimate,
+              condition,
+              eyeColor,
+              friendliness,
+              additionalComments,
+              pinID)
             alert('Submitted Successfully')
             return;
           }
