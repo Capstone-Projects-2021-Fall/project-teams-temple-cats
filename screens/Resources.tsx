38d21c7a44ab3ec0a2e88e21161404d49770ce18
Rebecca Robb <tuf87647@temple.edu>
2021-10-30 20:02:31 -0400

    Styling resources

--- screens/Resources.tsx
+++ screens/Resources.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
-import { Button, Linking, StyleSheet, Pressable } from "react-native";
-import Icon from 'react-native-vector-icons/FontAwesome';
+import { Image, FlatList, Linking, StyleSheet, Pressable,SafeAreaView, ScrollView } from "react-native";
 import { Text, View } from "../components/Themed";
 import { RootTabScreenProps } from "../types";
 
@@ -13,18 +12,67 @@ import { RootTabScreenProps } from "../types";
 export default function ResourcesScreen ({ navigation }: RootTabScreenProps<"Resources">) {
   const urlPaws="https://phillypaws.org/clinic-services/"
   const urlForgottenCats="https://forgottencats.org/program-services-fees/"
+  const pawsServices = [
+    {key: 'Spay/Neuter'},
+    {key: 'FVRCP'},
+    {key: 'Rabies'},
+    {key: 'Flea Meds'},
+    {key: 'Eartip'},
+    {key: 'Pain Medication'},
+  ]
+  const forgottenServices = [
+    {key: 'Spay/Neuter'},
+    {key: 'FVRCP'},
+    {key: 'Rabies'},
+    {key: 'Flea Meds'},
+    {key: 'Eartip'},
+    {key: 'Pain Medication'},
+    {key: 'Dewormer'},
+    {key: 'Penicillin Shot'},
+    {key: 'Overnight Holding'},
+  ]
+  
   return (
-    <View style={styles.container}>
-      <Pressable style={styles.button}
-        onPress={() => Linking.openURL(urlPaws)}>
-        <Text style={styles.text}>PAWS</Text>
-      </Pressable>
-
-      <Pressable style={styles.button}
-        onPress={() => Linking.openURL(urlForgottenCats)}>
-        <Text style={styles.text}>FORGOTTEN CATS</Text>
-      </Pressable>
-    </View>
+    <SafeAreaView>
+      <ScrollView >
+        <View style={styles.topCatButtonWrapper}>
+        <View style={styles.container}>
+        <Image
+          style = {styles.topCat}
+          source={require("../assets/images/resource-cat.png")}
+        />
+        </View>
+        <Pressable style={styles.button}
+          onPress={() => Linking.openURL(urlPaws)}>
+          <Text style={styles.text}>PAWS</Text>
+        </Pressable>
+        <View style={styles.listContainer}>
+          <FlatList
+            data={pawsServices}
+            renderItem={({item}) => <Text style={styles.listItem}>{'♥'} {item.key}</Text>}
+          />
+        </View>
+        <Pressable style={styles.button}
+          onPress={() => Linking.openURL(urlForgottenCats)}>
+          <Text style={styles.text}>FORGOTTEN CATS</Text>
+        </Pressable>
+        <View style={styles.bottomCatButtonWrapper}>
+          <FlatList
+            data={forgottenServices}
+            renderItem={({item}) => <Text style={styles.listItem}>{'♥'} {item.key}</Text>}
+          />
+          <Image
+          style = {{width:200, height: 300}}
+          source={require("../assets/images/resource-cat2.png")}
+        />
+        </View>
+        {/* <Image
+          style = {{width:200, height: 300}}
+          source={require("../assets/images/resource-cat2.png")}
+        /> */}
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )}
 
 const styles = StyleSheet.create({
@@ -36,6 +84,9 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     elevation: 3,
     backgroundColor: '#8B0000',
+    marginBottom: 20,
+    marginLeft: 20,
+    marginRight: 20,
   },
   text: {
     fontSize: 16,
@@ -46,8 +97,17 @@ const styles = StyleSheet.create({
   },
   container: {
     flex: 1,
-    alignItems: "center",
     justifyContent: "space-evenly"
+  },
+  listContainer: {
+      height: 200,
+      textAlign: 'left'
+  },
+  listItem: {
+    paddingTop: 10,
+    paddingBottom: 10,
+    marginLeft: 25,
+    marginRight: 25,
   },
   title: {
     fontSize: 20,
@@ -57,5 +117,17 @@ const styles = StyleSheet.create({
     marginVertical: 30,
     height: 1,
     width: "80%"
-  }
+  },
+  topCat: {
+    width:250, 
+    height: 150,
+    alignSelf: 'flex-end',
+    marginBottom: -25,
+  },
+  bottomCatButtonWrapper: {
+    height: 200,
+    textAlign: 'left',
+    display: 'flex',
+    flexDirection: "row"
+},
 });
