d4289823f98bef47f443ccf27fe056e5936bf809
Christine Gregotski <tuj39467@temple.edu>
2021-11-09 10:22:25 -0500

    Flatlist now renders when user types in search

--- components/Search.tsx
+++ components/Search.tsx
@@ -4,60 +4,57 @@ import { StyleSheet, Image, View, Text, FlatList, SafeAreaView, ScrollView } fro
 import {SearchBar} from "react-native-elements";
 import { Cat } from "../types";
 import { Searchbar } from "react-native-paper";
-import { Item } from "react-native-paper/lib/typescript/components/List/List";
 
 
 export default function Search () {
-  const currentData: Cat[] = [];
+  const currentData: Cat[] = []; //Used to initialize catData
   const catsRef = firebase.database().ref().child("Cats/");
-  const [search, setSearch] = useState("");
-  const [names, setCatNames] = useState([""]);
+  const [search, setSearch] = useState(""); //Stores the user's query
   const [catData, setCatData] = useState<Cat[]>([]); //Stores Cat data that is fetched from Firebase
-
+  const [data, setData] = useState<Cat[]>([]); //Stores the new data array to be rendered once the user types something in the search bar
   
-//Fetches data asynchronously from firebase to store in the results(Cat[]) so that it can be traversed within the search function by
-//filtering search bar text input to see if any of the input matches any element within the data array.
+/*Fetches data asynchronously from firebase to store in catData(Cat[]) hook array after it was first pushed to currentData (Cat[]) so that it can be traversed within the search function by
+filtering search bar text input to see if any of the input matches any element within the data array. */
 
   useEffect(() => {
     catsRef.on("child_added", async (snapshot) => {
+
       currentData.push({ ...snapshot.val() });
       setCatData([...currentData]);
     });
   }, []);
 
  
-  //This function will iterate through the entire results(Cat[]) array containing the Cat data from Firebase. While it traverses through 
-  //each element (Cat[]), another variable (defined as String) will store this element and then be used to initialize the data
-  //setData(newData) (setData (String, Cat names (item.name)), newData (Cat Name (String))). After it iterates through the results array, it will 
-  //traverse through the data array (array of strings), then it will check if the user input (text) matches elements within the data array.
+  /*This function will iterate through the entire catData hook array containing the Cat data from Firebase. While it traverses through 
+  each element (Cat[]), itemData (String) will read through each element and check if it contains the user input. The setData() (Cat[]) hook
+  will initialize newData (Cat[]) as the new data to be rendered by the flatlist when the user enters text within the search bar. */
   
   function searchFilterFunction(text){ 
-   // console.log(catData)
-
-    if(catData.includes(text)){
-      return catData.indexOf(text);   
-    } 
-    else{
-      //alert("Search not found");
-      return;
+  
+    let newData = catData.filter(item => {
+    const itemData = `${item.name}`;
+    const textData = text;
+    if(text.length > 0 ){
+      return itemData.indexOf(textData) > -1;
     }
+    });
+  
+    setSearch(text);
+    setData(newData);
   }
-   
-    
-//RECOMMENDED: add timer to not make it check the SearchFilterFunction every time a letter is entered
+   //Renders when the app renders so that the user can type something in
 function renderHeader(){
   return (
      <Searchbar
           style={{borderColor: "black", borderWidth: 2}}
           placeholder="Search for cats here"
-          onChangeText={(text) => {searchFilterFunction(text)
-          setSearch(text)}}
+          onChangeText={(text) => {searchFilterFunction(text)}}
           autoCorrect={false}
           value={search} onPressIn={true} onPressOut={true}               
       />     
   );
 };
-
+//Used to separate Cat data elements that come up during the user's search
 var renderSeparator = () => {
   return (
     <View
@@ -78,23 +75,21 @@ var renderSeparator = () => {
    
     
       
-        <FlatList          
-          data={catData}          
+        <FlatList      
+        //Flatlist renders when new data is pulled from the user's search query that comes from the renderHeader() function.    
+          data={data}          
           renderItem={({ item }) => ( 
-       //Right now this flatlist renders with the search bar even when the search bar is empty, but this should 
-       //only appear when the text is changed within the search bar.
-           
-              
+  
               <View style={{backgroundColor: "white"}}>
                 <Image
                   style={{ left: 10, top: 6, width: 40, height: 40, borderWidth: 4, borderColor: 'rgba(160, 28, 52, 0.75)', borderRadius: 7 }}
-                  source={{ uri: item.media }} /><Text style={styles.listItem}>{item.name} </Text>
+                  source={{ uri: item.media }} /><Text style={styles.listItem}>{item.name ? item.name : 'Unknown' }</Text>
               </View>
            
           )}          
         keyExtractor={item => item.catID}  
         ItemSeparatorComponent={renderSeparator} 
-        //Changed renderHeader() to be a function to allow the user to change text in search bar.
+        //renderHeader() is a function to allow the user to dynamically change text in search bar.
         ListHeaderComponent={renderHeader()}                             
         />            
    
