1869b13c5e557bacbb3a591ae724b99705151fb7
Christine Gregotski <tuj39467@temple.edu>
2021-11-08 22:26:29 -0500

    Improved flatlist appearance

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,13 +1,11 @@
-import { Ionicons } from "@expo/vector-icons";
 import firebase from "firebase";
 import React, {useEffect, useState} from "react";
-import { StyleSheet, ActivityIndicator, Image, View, TextInput, Text, Button, TouchableOpacity, StatusBar, FlatList, SafeAreaView, Pressable } from "react-native";
-import { ListItem, SearchBar, Avatar} from "react-native-elements";
-import MapView from "react-native-maps";
+import { StyleSheet, Image, View, Text, FlatList, SafeAreaView, ScrollView } from "react-native";
+import {SearchBar} from "react-native-elements";
 import { Cat } from "../types";
 import { Searchbar } from "react-native-paper";
-//import { Avatar} from "react-native-ui-kitten";
-import { placeholder } from "@babel/types";
+import { Item } from "react-native-paper/lib/typescript/components/List/List";
+
 
 export default function Search () {
   const currentData: Cat[] = [];
@@ -15,63 +13,32 @@ export default function Search () {
   const [search, setSearch] = useState("");
   const [names, setCatNames] = useState([""]);
   const [catData, setCatData] = useState<Cat[]>([]); //Stores Cat data that is fetched from Firebase
-  //const [results, setResults] = useState<Cat[]>([]); 
 
   
 //Fetches data asynchronously from firebase to store in the results(Cat[]) so that it can be traversed within the search function by
 //filtering search bar text input to see if any of the input matches any element within the data array.
-/*useEffect(() => {
-  
-    catsRef.on("child_added", async (snapshot) => {
-    
-      snapshot.forEach(function(data) {
-        
-        currentData.push(data.val());
-        setCatData([...currentData])
-    });
-  });
-  }, []);*/
+
   useEffect(() => {
     catsRef.on("child_added", async (snapshot) => {
-      const picUri = await firebase
-        .storage()
-        .ref()
-        .child(snapshot.val().accountID + "/" + snapshot.val().catID + "/")
-        .getDownloadURL();
-      currentData.push({ ...snapshot.val(), media: picUri });
-
+      currentData.push({ ...snapshot.val() });
       setCatData([...currentData]);
     });
   }, []);
 
  
- /* fetch(
-    `https://temple-cats-default-rtdb.firebaseio.com/Cats.json`
-    ).then(res => res.json())
-    .then(res => {
-       
-        currentData.push(res)
-        setResults([...currentData]);
-        // console.log(currentData);
-    })
-    .catch((err) => { console.log(err)  
-    });
-  */
-  //});
-//}
   //This function will iterate through the entire results(Cat[]) array containing the Cat data from Firebase. While it traverses through 
   //each element (Cat[]), another variable (defined as String) will store this element and then be used to initialize the data
   //setData(newData) (setData (String, Cat names (item.name)), newData (Cat Name (String))). After it iterates through the results array, it will 
   //traverse through the data array (array of strings), then it will check if the user input (text) matches elements within the data array.
   
   function searchFilterFunction(text){ 
-    console.log(catData)
+   // console.log(catData)
 
     if(catData.includes(text)){
-      return catData.indexOf(text) > -1;   
+      return catData.indexOf(text);   
     } 
     else{
-      alert("Search not found");
+      //alert("Search not found");
       return;
     }
   }
@@ -81,6 +48,7 @@ export default function Search () {
 function renderHeader(){
   return (
      <Searchbar
+          style={{borderColor: "black", borderWidth: 2}}
           placeholder="Search for cats here"
           onChangeText={(text) => {searchFilterFunction(text)
           setSearch(text)}}
@@ -95,7 +63,7 @@ var renderSeparator = () => {
     <View
       style={{
         height: 1,
-        width: '86%',
+        width: 20,
         backgroundColor: '#CED0CE',
         marginLeft: '14%',
       }}
@@ -105,8 +73,9 @@ var renderSeparator = () => {
 
   return ( 
   <SafeAreaView style={{ flex: 1 }}>
-    <View style={{left: -190, position: 'absolute', top: -5, width: "90%", borderColor: "black",flexDirection: 'row', alignItems: "center"}}>
     
+    <View style={{left: -190, position: 'absolute', top: -5, width: "90%", borderColor: "black",flexDirection: 'row', alignItems: "center"}}>
+   
     
       
         <FlatList          
@@ -115,12 +84,12 @@ var renderSeparator = () => {
        //Right now this flatlist renders with the search bar even when the search bar is empty, but this should 
        //only appear when the text is changed within the search bar.
            
-              <>
               
-              <Image
-                style={{left: 10, width: 40, height: 40, borderWidth: 4, borderColor: 'rgba(160, 28, 52, 0.75)', borderRadius: 7 }}
-                source={{ uri: item.media }} /><Text style={styles.listItem}>{item.name}</Text></>
-        
+              <View style={{backgroundColor: "white"}}>
+                <Image
+                  style={{ left: 10, top: 6, width: 40, height: 40, borderWidth: 4, borderColor: 'rgba(160, 28, 52, 0.75)', borderRadius: 7 }}
+                  source={{ uri: item.media }} /><Text style={styles.listItem}>{item.name} </Text>
+              </View>
            
           )}          
         keyExtractor={item => item.catID}  
@@ -128,8 +97,9 @@ var renderSeparator = () => {
         //Changed renderHeader() to be a function to allow the user to change text in search bar.
         ListHeaderComponent={renderHeader()}                             
         />            
-  
+   
    </View>
+  
  </SafeAreaView>
 );  
 }
@@ -144,15 +114,15 @@ const styles = StyleSheet.create({
   },
   listItem: {
     alignItems: "center",
-    width: 280,
-    paddingTop: 8,
-    paddingBottom: 5,
+    width: 230,
+    paddingTop: 1,
+    paddingBottom: 1,
     marginLeft: 275,
-    top: -37,
+    top: -30,
     flexDirection: "row",
     left: -220,
     backgroundColor: "white",
-    borderColor: "grey",
+    borderColor: "white",
     borderWidth: 1
   },
   itemStyle: {
@@ -168,31 +138,3 @@ const styles = StyleSheet.create({
     marginBottom: 5
   },
 });
-/* 
-
- <Searchbar
-          placeholder="Search for cats here"
-          onChangeText={(text) => searchFilterFunction(text)}
-            
-          value={search} onPressIn={undefined} onPressOut={undefined}               
-      />     
- <ListItem   
-                               
-            title={item.name}  
-            subtitle={item.name}                           
-            avatar={{ uri: item.media }}   
-            containerStyle={{ borderBottomWidth: 0 }} 
-          />      
-
-
- <View style={{ borderTopWidth: 0, borderBottomWidth: 0 }}>
- // </View>
-
-
-
-<View style={{backgroundColor: "red", height: 10, width: 0, right: 460, borderColor: "black"}}>
-
-
-//{catData.map(item => <Text style={styles.listItem}>{catData[1]}</Text>)}
-
-*/
