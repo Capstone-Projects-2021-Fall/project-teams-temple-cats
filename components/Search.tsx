e669b1edc7c2641a417656cb53fa8c3e82e4a5bf
Christine Gregotski <tuj39467@temple.edu>
2021-11-09 14:34:01 -0500

    made list item tappable but cant go to location

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,9 +1,13 @@
 import firebase from "firebase";
-import React, {useEffect, useState} from "react";
-import { StyleSheet, Image, View, Text, FlatList, SafeAreaView, ScrollView } from "react-native";
+import React, {useEffect, useState,  useRef} from "react";
+import MapView, { LatLng, Marker } from "react-native-maps";
+import { StyleSheet, Image, View, Text, FlatList, SafeAreaView, ScrollView, Pressable, Button } from "react-native";
 import {SearchBar} from "react-native-elements";
 import { Cat } from "../types";
 import { Searchbar } from "react-native-paper";
+import Gps from "../utils/gps";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import CatCamera from "./Camera";
 
 
 export default function Search () {
@@ -12,14 +16,26 @@ export default function Search () {
   const [search, setSearch] = useState(""); //Stores the user's query
   const [catData, setCatData] = useState<Cat[]>([]); //Stores Cat data that is fetched from Firebase
   const [data, setData] = useState<Cat[]>([]); //Stores the new data array to be rendered once the user types something in the search bar
-  
+  const mapViewRef: React.MutableRefObject<MapView> | React.MutableRefObject<null> = useRef(null);
+  const catLocation = {latitude: LatLng,
+    longitude: LatLng,
+    latitudeDelta 0.022,
+    longitudeDelta: 0.022,
+   };
+  const [location, setLocation] = useState(Gps());
+  const myLocation = Gps();
+
 /*Fetches data asynchronously from firebase to store in catData(Cat[]) hook array after it was first pushed to currentData (Cat[]) so that it can be traversed within the search function by
 filtering search bar text input to see if any of the input matches any element within the data array. */
 
   useEffect(() => {
     catsRef.on("child_added", async (snapshot) => {
-
-      currentData.push({ ...snapshot.val() });
+      const picUri = await firebase
+      .storage()
+      .ref()
+      .child(snapshot.val().accountID + "/" + snapshot.val().catID + "/");
+      currentData.push({ ...snapshot.val()});
+    
       setCatData([...currentData]);
     });
   }, []);
@@ -68,22 +84,32 @@ var renderSeparator = () => {
   );
 };
 
+function goToCat() {
+  mapViewRef.current?.animateToRegion(
+    
+    catLocation,
+  
+    
+    1000,
+  );
+}
+
   return ( 
   <SafeAreaView style={{ flex: 1 }}>
     
     <View style={{left: -190, position: 'absolute', top: -5, width: "90%", borderColor: "black",flexDirection: 'row', alignItems: "center"}}>
    
-    
-      
         <FlatList      
         //Flatlist renders when new data is pulled from the user's search query that comes from the renderHeader() function.    
           data={data}          
           renderItem={({ item }) => ( 
-  
-              <View style={{backgroundColor: "white"}}>
+             
+              <View style={{backgroundColor: "white"}} {...catLocation.longitude = item.location.longitude } {...catLocation.latitude = item.location.latitude}>
+                <TouchableOpacity onPress={goToCat}>
                 <Image
                   style={{ left: 10, top: 6, width: 40, height: 40, borderWidth: 4, borderColor: 'rgba(160, 28, 52, 0.75)', borderRadius: 7 }}
                   source={{ uri: item.media }} /><Text style={styles.listItem}>{item.name ? item.name : 'Unknown' }</Text>
+                  </TouchableOpacity>
               </View>
            
           )}          
@@ -119,6 +145,9 @@ const styles = StyleSheet.create({
     backgroundColor: "white",
     borderColor: "white",
     borderWidth: 1
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
   },
   itemStyle: {
     padding: 10,
