446b58ab2085844e2766e066590df435e42680b7
Christine Gregotski <tuj39467@temple.edu>
2021-11-10 19:54:16 -0500

    Code clean up

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,10 +1,8 @@
 import firebase from "firebase";
-import React, {useEffect, useState,  useRef} from "react";
-import { StyleSheet, Image, View, Text, FlatList, SafeAreaView, ScrollView, Pressable, Button } from "react-native";
-import {SearchBar} from "react-native-elements";
+import React, {useEffect, useState } from "react";
+import { StyleSheet, Image, View, Text, FlatList, SafeAreaView } from "react-native";
 import { Cat } from "../types";
 import { Searchbar } from "react-native-paper";
-import Gps from "../utils/gps";
 import { TouchableOpacity } from "react-native-gesture-handler";
 
 
@@ -14,16 +12,7 @@ export default function Search ({mapViewRef}){
   const [search, setSearch] = useState(""); //Stores the user's query
   const [catData, setCatData] = useState<Cat[]>([]); //Stores Cat data that is fetched from Firebase
   const [data, setData] = useState<Cat[]>([]); //Stores the new data array to be rendered once the user types something in the search bar
-  //const mapViewRef: React.MutableRefObject<MapView> | React.MutableRefObject<null> = useRef(null);
-  let catLocation : {
-    latitude: Number;
-    longitude: Number;
-    latitudeDelta: 0.022;
-    longitudeDelta: 0.022;
-   }
-  const [locationLat, setLocationLat] = useState(Number);
-  const [locationLng, setLocationLng] = useState(Number);
-  const myLocation = Gps();
+ 
 
 /*Fetches data asynchronously from firebase to store in catData(Cat[]) hook array after it was first pushed to currentData (Cat[]) so that it can be traversed within the search function by
 filtering search bar text input to see if any of the input matches any element within the data array. */
@@ -40,10 +29,10 @@ filtering search bar text input to see if any of the input matches any element w
   each element (Cat[]), itemData (String) will read through each element and check if it contains the user input. The setData() (Cat[]) hook
   will initialize newData (Cat[]) as the new data to be rendered by the flatlist when the user enters text within the search bar. */
   
-  function searchFilterFunction(text: any[] | React.SetStateAction<string>){ 
-  
+  function searchFilterFunction(text){ 
+    
     let newData = catData.filter(item => {
-    const itemData = `${item.name} ${item.healthy}` ;
+    const itemData = `${item.name}` ;
     const textData = text;
     if(text.length > 0 ){
       return itemData.indexOf(textData) > -1;
@@ -73,7 +62,7 @@ var renderSeparator = () => {
         height: 1,
         width: 20,
         backgroundColor: '#CED0CE',
-        marginLeft: '14%',
+        marginLeft: '14%'
       }}
     />
   );
@@ -95,23 +84,20 @@ function goToCat(lat, lng) {
 
   return ( 
   <SafeAreaView style={{ flex: 1 }}>
-    
     <View style={{left: -190, position: 'absolute', top: -5, width: "90%", borderColor: "black",flexDirection: 'row', alignItems: "center"}}>
-   
         <FlatList      
         //Flatlist renders when new data is pulled from the user's search query that comes from the renderHeader() function.    
           data={data}          
-          renderItem={({ item }) => ( //setLocationLng(item.location.longitude), setLocationLat(item.location.latitude),
+          renderItem={({ item }) => ( 
              
-              <View style={{backgroundColor: "white"}} >
-                
-                <TouchableOpacity onPress={() => {goToCat(item.location.latitude,item.location.longitude) }} >
+              <View style={{backgroundColor: "white", paddingBottom: -10}} >
+                <TouchableOpacity onPress={() => { goToCat(item.location.latitude, item.location.longitude) }} >
                 <Image
                   style={{ left: 10, top: 6, width: 40, height: 40, borderWidth: 4, borderColor: 'rgba(160, 28, 52, 0.75)', borderRadius: 7 }}
-                  source={{ uri: item.media }} /><Text style={styles.listItem}>{item.name ? item.name : 'Unknown' }</Text>
+                  source={{ uri: item.media }} />
+                  <Text style={styles.listItem}> {item.name ? item.name : 'Unknown' }</Text>
                   </TouchableOpacity>
               </View>
-           
           )}          
         keyExtractor={item => item.catID}  
         ItemSeparatorComponent={renderSeparator} 
@@ -120,7 +106,6 @@ function goToCat(lat, lng) {
         />            
    
    </View>
-  
  </SafeAreaView>
 );  
 }
@@ -134,10 +119,11 @@ const styles = StyleSheet.create({
       justifyContent: "center"
   },
   listItem: {
-    alignItems: "center",
-    width: 230,
+    fontSize: 17,
+    fontWeight: "bold", 
+    width: 220,
     paddingTop: 1,
-    paddingBottom: 1,
+    paddingBottom: -2,
     marginLeft: 275,
     top: -30,
     flexDirection: "row",
