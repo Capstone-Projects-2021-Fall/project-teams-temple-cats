afcef9474036367f04ca233a0c34fd4d4e91cb86
Karl Schaller <tui98107@temple.edu>
2021-11-19 22:49:29 -0500

    finally getting emulator

--- __tests__/dbInterface-test.tsx
+++ __tests__/dbInterface-test.tsx
@@ -1,48 +1,37 @@
-import firebase from '../utils/firebase';
+import firebase from 'firebase';
+import "firebase/database";
 
-// const firebase = require('@firebase/testing')
-// const admin = require('firebase-admin')
+const firebaseConfig = {
+  apiKey: "AIzaSyDhKqRV1ALg80TdK0GsSFEq0BR6BK8UiPs",
+  authDomain: "temple-cats.firebaseapp.com",
+  databaseURL: "http://localhost:9000/?ns=temple-cats",
+  projectId: "temple-cats",
+  storageBucket: "temple-cats.appspot.com",
+  messagingSenderId: "645364453008",
+  appId: "1:645364453008:web:e2b6afd21de03516ccd215",
+  measurementId: "G-PTQHR7RT4M",
+  ssl: false
+};
 
-import { addUser } from '../utils/dbInterface';
-
-// const projectId = 'test';
-// process.env.GCLOUD_PROJECT = projectId;
-// process.env.FIRESTORE_EMULATOR_HOST = 'localhost:8080';
-// let app = admin.initializeApp();
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDhKqRV1ALg80TdK0GsSFEq0BR6BK8UiPs",
-//   authDomain: "temple-cats.firebaseapp.com",
-//   databaseURL: "https://temple-cats-default-rtdb.firebaseio.com",
-//   projectId: "temple-cats",
-//   storageBucket: "temple-cats.appspot.com",
-//   messagingSenderId: "645364453008",
-//   appId: "1:645364453008:web:e2b6afd21de03516ccd215",
-//   measurementId: "G-PTQHR7RT4M"
-// };
-
-// if (firebase.apps.length === 0) firebase.initializeApp(firebaseConfig);
-
+const app = firebase.initializeApp(firebaseConfig);
 let db = firebase.database();
-db.useEmulator('localhost', 9000);
-
-jest.mock('../utils/firebase', () => {
-  const set = jest.fn();
-  return {
-    database: jest.fn(() => ({
-      ref: jest.fn(() => ({
-        child: jest.fn(() => ({
-          set,
-        })),
-      })),
-    })),
-  };
-});
  
 describe('DB Interface', () => {
+
+  beforeAll(() => {
+    db.ref().set(null);
+  })
+
+  it('Adds a cat', async () => {
+    await expect(1).toEqual(1);
+  })
+
+  it('Adds a picture', async() => {
+    await expect(1).toEqual(1);
+  })
+
   it('Adds a user', async () => {
-    const set = firebase
-      .database()
+    const set = db
       .ref()
       .child(`Accounts/krs8Lf6EXbb9GTBmQlaj4nvx1CA2`)
       .set({
@@ -53,16 +42,9 @@ describe('DB Interface', () => {
         posts: 0,
         modStatus: 1,
         banStatus: false,
-      });
-     
-    // const result = addUser(
-    //   'Jordan Billie',
-    //   'krs8Lf6EXbb9GTBmQlaj4nvx1CA2',
-    //   'bjthaniel3@hotmail.com',
-    //   'https://graph.facebook.com/2544322639045275/picture'
-    // );
+    });
  
-   await expect(set).resolves.toEqual("true");    
+     await expect(set).resolves.toEqual(undefined);    
   });
 });
 
