9fc6df69886ef2c9004484ce97256681e7066201
Karl Schaller <tui98107@temple.edu>
2021-11-19 21:10:06 -0500

    Trying emulator stuff

--- __tests__/dbInterface-test.tsx
+++ __tests__/dbInterface-test.tsx
@@ -1,7 +1,30 @@
-import React from 'react';
-import { addUser } from '../utils/dbInterface';
 import firebase from '../utils/firebase';
 
+// const firebase = require('@firebase/testing')
+// const admin = require('firebase-admin')
+
+import { addUser } from '../utils/dbInterface';
+
+// const projectId = 'test';
+// process.env.GCLOUD_PROJECT = projectId;
+// process.env.FIRESTORE_EMULATOR_HOST = 'localhost:8080';
+// let app = admin.initializeApp();
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDhKqRV1ALg80TdK0GsSFEq0BR6BK8UiPs",
+//   authDomain: "temple-cats.firebaseapp.com",
+//   databaseURL: "https://temple-cats-default-rtdb.firebaseio.com",
+//   projectId: "temple-cats",
+//   storageBucket: "temple-cats.appspot.com",
+//   messagingSenderId: "645364453008",
+//   appId: "1:645364453008:web:e2b6afd21de03516ccd215",
+//   measurementId: "G-PTQHR7RT4M"
+// };
+
+// if (firebase.apps.length === 0) firebase.initializeApp(firebaseConfig);
+
+let db = firebase.database();
+db.useEmulator('localhost', 9000);
 
 jest.mock('../utils/firebase', () => {
   const set = jest.fn();
@@ -32,14 +55,14 @@ describe('DB Interface', () => {
         banStatus: false,
       });
      
-    const result = addUser(
-      'Jordan Billie',
-      'krs8Lf6EXbb9GTBmQlaj4nvx1CA2',
-      'bjthaniel3@hotmail.com',
-      'https://graph.facebook.com/2544322639045275/picture'
-    );
+    // const result = addUser(
+    //   'Jordan Billie',
+    //   'krs8Lf6EXbb9GTBmQlaj4nvx1CA2',
+    //   'bjthaniel3@hotmail.com',
+    //   'https://graph.facebook.com/2544322639045275/picture'
+    // );
  
-   await expect(result).resolves.toEqual("true");    
+   await expect(set).resolves.toEqual("true");    
   });
 });
 
