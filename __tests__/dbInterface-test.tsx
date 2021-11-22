aec3a53f3c98c273c3e599c7be3e9fe2fec1aa5d
Karl Schaller <tui98107@temple.edu>
2021-11-21 19:47:12 -0500

    Add skeleton code for tests

--- __tests__/dbInterface-test.tsx
+++ __tests__/dbInterface-test.tsx
@@ -13,38 +13,137 @@ const firebaseConfig = {
   ssl: false
 };
 
-const app = firebase.initializeApp(firebaseConfig);
-let db = firebase.database();
+firebase.initializeApp(firebaseConfig);
+const dbRef = firebase.database().ref();
  
 describe('DB Interface', () => {
 
   beforeAll(() => {
-    db.ref().set(null);
+    dbRef.set(null);
   })
 
   it('Adds a cat', async () => {
-    await expect(1).toEqual(1);
-  })
+    const set = dbRef
+      .child(`Cats/cat1`)
+      .set({
+        accountID: 'acc1',
+        catID: 'cat1',
+        color: 'White',
+        date: '11/9/2021',
+        eyeColor: 'Green',
+        friendly: false,
+        healthy: false,
+        kitten: false,
+        location: {latitude: 39.98446100629369, longitude: -75.15069779008627},
+        media: 'TODO',
+        name: 'Test Cat',
+        time: '9:23:00 AM',
+        votes: 0
+    });
+     await expect(set).resolves.toEqual(undefined);
+  });
 
-  it('Adds a picture', async() => {
+  it('Gets cats', async() => {
     await expect(1).toEqual(1);
-  })
+  });
+
+  it('Changes upvotes of cat', async() => {
+    await expect(1).toEqual(1);
+  });
+
+  it('Adds report to a cat', async() => {
+    await expect(1).toEqual(1);
+  });
+
+  it('Gets reports of a cat', async() => {
+    await expect(1).toEqual(1);
+  });
+
+  it('Removes reports of a cat', async() => {
+    await expect(1).toEqual(1);
+  });
+
+  it('Adds a comment to a cat', async() => {
+    await expect(1).toEqual(1);
+  });
+
+  it('Gets a cat\'s comments', async() => {
+    await expect(1).toEqual(1);
+  });
+
+  it('Adds report to a cat\'s comment', async() => {
+    await expect(1).toEqual(1);
+  });
+
+  it('Gets reports of a cat\'s comment', async() => {
+    await expect(1).toEqual(1);
+  });
+
+  it('Removes report on a cat\'s comment', async() => {
+    await expect(1).toEqual(1);
+  });
+
+  it('Removes a cat\'s comment', async() => {
+    await expect(1).toEqual(1);
+  });
+
+  it('Removes a cat', async() => {
+    await expect(1).toEqual(1);
+  });
+
+  it('Gets feeding stations', async() => {
+    await expect(1).toEqual(1);
+  });
 
   it('Adds a user', async () => {
-    const set = db
-      .ref()
-      .child(`Accounts/krs8Lf6EXbb9GTBmQlaj4nvx1CA2`)
+    const set = dbRef
+      .child(`Accounts/acc1`)
       .set({
         display: 'Jordan Billie',
-        accountID: 'krs8Lf6EXbb9GTBmQlaj4nvx1CA2',
+        accountID: 'acc1',
         email: 'bjthaniel3@hotmail.com',
         photo: 'https://graph.facebook.com/2544322639045275/picture',
         posts: 0,
         modStatus: 1,
         banStatus: false,
     });
-
      await expect(set).resolves.toEqual(undefined);
+  });
+
+  it('Gets users', async() => {
+    await expect(1).toEqual(1);
+  });
+
+  it('Changes a user\'s score', async() => {
+    await expect(1).toEqual(1);
+  });
+
+  it('Removes a user', async() => {
+    await expect(1).toEqual(1);
+  });
+
+  it('Adds an announcement', async() => {
+    await expect(1).toEqual(1);
+  });
+
+  it('Gets announcements', async() => {
+    await expect(1).toEqual(1);
+  });
+
+  it('Removes an announcement', async() => {
+    await expect(1).toEqual(1);
+  });
+
+  it('Uploads a cat\'s picture', async() => {
+    await expect(1).toEqual(1);
+  });
+
+  it('Gets a cat\'s picture', async() => {
+    await expect(1).toEqual(1);
+  });
+
+  it('Deletes a cat\'s picture', async() => {
+    await expect(1).toEqual(1);
   });
 });
 
